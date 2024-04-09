c63843c07283d147e65695172dd49b3f3db8503d
Stephanie Carr <sacarr91@gmail.com>
2024-04-09 18:28:43 -0400

    post-tutoring session with Sandy

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,43 @@
-// add blog.css to head
-document.head.append(``)
+//pull data from local storage
+//display in cards
+//  with author name, title, post
 
-let entryCard = document.createElement('div');
-// entryCard 
+const blogEntries = JSON.parse(localStorage.getItem("entry")) || [] 
+
+//create postcard function
+
+//iterate through array, call function to display/append entry card
+function renderBlogPage() {
+//declare variables from html
+
+for (let i = 0; i < blogEntries.length; i++) {
+    const blog = blogEntries[i];
+    console.log(blog);
+    //call postcard function
+}
+}
+
+const cardContainer = document.getElementById('card-container');
+
+function newPostCard(title, text) {
+    const postCard = document.createElement('div');
+    postCardard.setAttribute('class', 'display-card');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.textContent = title;
+
+    const cardContent = document.createElement('p');
+    cardContent.textContent = content;
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardContent);
+
+    return card;
+}
+
+// Example usage
+const card1 = createCard('Dynamic Cards 101', 'Learn how to spice up your web page with dynamic card rendering!');
+const card2 = createCard('The Power of Flexibility', 'Discover the endless possibilities of dynamic content.');
+
+cardContainer.appendChild(card1);
+cardContainer.appendChild(card2);
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,30 @@
-let blogEntries = []
+const blogEntries = []
 
-function addToSeries() {
-    blogEntries.push(document.getElementById("new-post").value);
+// purpose: gather value of user input, group together as object, add to array, store in local storage, final step: redirect to blog.html
+function addToSeries(e) {
+    e.preventDefault();
+
+    const author = document.getElementById("author").value.trim();
+    const entryTitle = document.getElementById("entry-title").value.trim();
+    const postContent = document.getElementById("new-post-content").value.trim();
+    
+    let blogPost = {
+        author: author,
+        entryTitle: entryTitle,
+        postContent: postContent
+    }
+    console.log(blogPost)
+
+    // add to array
+    blogEntries.push(blogPost);
     console.log(`Updated array: ${blogEntries}`)
+
+    
+    // store in local storage
+    localStorage.setItem("entry", JSON.stringify(blogEntries));
+
+    //redirect to blog.html
+    location.href = './blog.html'
 }
 
 document.getElementById(`submit-button`).addEventListener("click", addToSeries)
