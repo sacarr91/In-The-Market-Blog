e002b731208ad7a08e223a45714f33d5ced67257
Stephanie Carr <sacarr91@gmail.com>
2024-04-12 23:14:28 -0400

    updated images for dark/light mode toggle

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,20 @@
 const toggle = document.getElementById("flexSwitchCheckDefault")
+const headEl = document.getElementById("Stanley");
+const logoImg = document.getElementById("logo");
+const sunImg = document.getElementById("sun");
+const moonImg = document.getElementById("moon");
 
 function changeColorMode() {
     if (toggle.checked) {
-        document.getElementById("Stanley").setAttribute("data-bs-theme", "dark");
+        headEl.setAttribute("data-bs-theme", "dark");
+        logoImg.setAttribute("src", "./assets/in the market logo-dk.png");
+        sunImg.setAttribute("src", "./assets/sun-dk.png");
+        moonImg.setAttribute("src", "./assets/moon-dk.png");
     } else {
-        document.getElementById("Stanley").removeAttribute("data-bs-theme", "dark");
+        headEl.removeAttribute("data-bs-theme", "dark");
+        logoImg.setAttribute("src", "./assets/in the market logo-lt.png");
+        sunImg.setAttribute("src", "./assets/sun-lt.png");
+        moonImg.setAttribute("src", "./assets/moon-lt.png");
     }
 }
 
