83d44d2175e92750aadd20d53bc06970c2ed6196
Stephanie Carr <sacarr91@gmail.com>
2024-04-14 23:23:07 -0400

    updated functions to render blog posts on cards

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,56 +1,52 @@
 //pull data from local storage
 //display in cards
 //  with author name, title, post
+const cardContainer = document.getElementById('card-container');
+const allEntries = JSON.parse(localStorage.getItem('blogEntries')) || [];
 
-const blogEntries = JSON.parse(localStorage.getItem("entry")) || [] 
+function newPostCard(x, y, z) {
+    const fancyCard = document.createElement('div');
+    fancyCard.setAttribute('class', 'card')
+    const fancyBox = document.createElement('div');
+    fancyBox.setAttribute('class', 'light-box')
+    const postCard = document.createElement('div');
+    postCard.setAttribute('class', 'display-card');
 
-// test --remove after
-for (let i = 0; i < blogEntries.length; i++) {
-    const blog = blogEntries[i];
-    console.log(blog);}
-// remove above
+    const postTitle = document.createElement('h2');
+    postTitle.textContent = x;
+
+    const postAuthor = document.createElement('h3');
+    postAuthor.textContent = `by ${y}`;
+
+    const postContent = document.createElement('p');
+    postContent.textContent = z;
+
+    postCard.appendChild(postTitle);
+    postCard.appendChild(postAuthor);
+    postCard.appendChild(postContent);
+
+    fancyBox.appendChild(postCard);
+    fancyCard.appendChild(fancyBox);
+
+    cardContainer.appendChild(fancyCard);
+}
+
+for (let i = 0; i < allEntries.length; i++) {
+    const blogAuthor = allEntries[i].author;
+    const blogTitle = allEntries[i].entryTitle;
+    const blogContent = allEntries[i].postContent;
+    newPostCard(blogTitle, blogAuthor, blogContent);
+}
+
+//declare variables from html
+
+
+
+//iterate through array, call function to display/append entry card
 
 //create postcard function
 
-//iterate through array, call function to display/append entry card
-// function renderBlogPage() {
-// //declare variables from html
 
-// // let blogPost = {
-// //     author: author,
-// //     entryTitle: entryTitle,
-// //     postContent: postContent
-// }
-
-// for (let i = 0; i < blogEntries.length; i++) {
-//     const blog = blogEntries[i];
-//     console.log(blog);
-//     // newPostCard();
-// }
-// }
-
-// const cardContainer = document.getElementById('card-container');
-
-// function newPostCard(entryTitle, author, postContent) {
-//     const postCard = document.createElement('div');
-//     postCard.setAttribute('class', 'display-card');
-
-//     const cardTitle = document.createElement('h2');
-//     cardTitle.textContent = title;
-
-//     const cardAuthor = document.createElement('h3');
-//     cardAuthor.textContent = author;
-
-//     const cardContent = document.createElement('p');
-//     cardContent.textContent = content;
-
-//     postCard.appendChild(cardTitle);
-//     postCard.appendChild(cardContent);
-
-//     return postCard;
-// }
-
-// // Example usage
 // // const card1 = createCard('Dynamic Cards 101', 'Learn how to spice up your web page with dynamic card rendering!');
 // // const card2 = createCard('The Power of Flexibility', 'Discover the endless possibilities of dynamic content.');
 
